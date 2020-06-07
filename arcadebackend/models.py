c0d1e1fcbc1920d2f93ab8c696a63201eaa03a34
atwelleric <atwelleric@gmail.com>
2020-06-07 17:13:53 -0500

    game model added, and able to route to api

--- arcadebackend/models.py
+++ arcadebackend/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=120)
+    img_source = models.URLField()
+    about = models.TextField()
+    creator_info = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    game = models.ForeignKey(
+        Game, on_delete=models.CASCADE, related_name='comments')
+    body = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.body
--- arcadebackend/views.py
+++ arcadebackend/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import GameSerializer
+from .models import Game, Comment
 
-# Create your views here.
+
+class GameList(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+
+class GameDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
